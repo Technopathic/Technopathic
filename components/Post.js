fb5a6a65e7aaba687aacbc001e853c58350787bf
Ren <nsoharab@gmail.com>
2021-03-11 23:27:14 +0200

    Trying to fix social images

--- components/Post.js
+++ components/Post.js
@@ -5,16 +5,13 @@ const Post = ({ children, meta }) => (
   <>
     <Head>
       <title>{`${meta.title} :: ${process.env.APP_TITLE}`} </title>
-      <meta name="Description" content={meta.description} />
+      <meta name="description" content={meta.description} />
 
-      <meta property="og:title" content={`${meta.title} :: ${process.env.APP_SITE_NAME}`} />
-      <meta property="og:description" content={meta.description} />
+      <meta name="og:title" property="og:title" content={`${meta.title} :: ${process.env.APP_SITE_NAME}`} />
+      <meta name="og:description" property="og:description" content={meta.description} />
 
-      <meta property="og:image" content={`${process.env.APP_URL}/${meta.coverImage}`} />
-      <meta property="og:image:secure_url" content={`${process.env.APP_URL}/${meta.coverImage}`} />
-
-      <meta property="og:image:width" content="256" />
-      <meta property="og:image:height" content="256" />
+      <meta name="og:image" property="og:image" content={`${process.env.APP_URL}/${meta.coverImage}`} />
+      <meta name="og:image:secure_url" property="og:image:secure_url" content={`${process.env.APP_URL}/${meta.coverImage}`} />
 
     </Head>
     <div className="flex flex-col w-full max-w-screen-lg mx-auto mt-24">
--- pages/_document.js
+++ pages/_document.js
@@ -11,14 +11,14 @@ class MyDocument extends Document {
                     <link rel="icon" type="image/png" sizes="16x16" href={process.env.APP_FAVICON_16x16} />
                     <link rel="icon" type="image/png" sizes="32x32" href={process.env.APP_FAVICON_32x32} />
 
-                    <meta property="og:title" content={process.env.APP_TITLE} />
+                    <meta name="og:title" property="og:title" content={process.env.APP_TITLE} />
                     <meta property="og:site_name" content={process.env.APP_SITE_NAME} />
                     <meta property="og:locale" content={process.env.APP_LOCALE} />
                     <meta property="og:type" content={process.env.APP_TYPE} />
-                    <meta property="og:description" content={process.env.APP_DESCRIPTION} />
-                    <meta property="og:url" content={process.env.APP_URL} />
-                    <meta property="og:image" content={process.env.APP_IMAGE} />
-                    <meta property="og:image:secure_url" content={process.env.APP_IMAGE} />
+                    <meta name="og:description" property="og:description" content={process.env.APP_DESCRIPTION} />
+                    <meta name="og:url" property="og:url" content={process.env.APP_URL} />
+                    <meta name="og:image" property="og:image" content={process.env.APP_IMAGE} />
+                    <meta name="og:image:secure_url" property="og:image:secure_url" content={process.env.APP_IMAGE} />
                     <meta property="og:image:type" content={process.env.APP_IMAGE_TYPE} />
                     <meta property="og:image:width" content={process.env.APP_IMAGE_WIDTH} />
                     <meta property="og:image:height" content={process.env.APP_IMAGE_HEIGHT} />

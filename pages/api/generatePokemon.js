87b92da56ea37b47cd806b2a79f6b6edbb7cd1a0
Ren <nsoharab@gmail.com>
2021-08-14 01:44:35 +0300

    WIthout string

--- pages/api/generatePokemon.js
+++ pages/api/generatePokemon.js
@@ -66,7 +66,7 @@ export default async (req, res) => {
         console.log(`Verification Failed: timestamp > 10 minutes. Message Id: ${messageId}.`)
         return res.status(401).json({ error: 'Not Allowed' })
     }
-    const computedSignature = "sha256=" + crypto.createHmac("sha256", process.env.TWITCH_HUB_SECRET).update(messageID + messageTime + Buffer.from(req.body).toString('utf8')).digest("hex")
+    const computedSignature = "sha256=" + crypto.createHmac("sha256", process.env.TWITCH_HUB_SECRET).update(messageID + messageTime + Buffer.from(req.body)).digest("hex")
 
     console.log(messageSignature)
     console.log(computedSignature)

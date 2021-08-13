3557ef12df19c4edf1d69c85c9ae5b53b0cda8dd
Ren <nsoharab@gmail.com>
2021-08-13 11:46:37 +0300

    Twitch Token

--- next.config.js
+++ next.config.js
@@ -26,7 +26,9 @@ const env = {
     "DISCORD": "https://discord.gg/XTkwtcxW",
     "KO_FI": "https://ko-fi.com/nownano",
     "SUPABASE_KEY": "",
-    "SUPABASE_URL": ""
+    "SUPABASE_URL": "",
+    "TWITCH_ID": "e9p3hp00halu9nm0gfs3m0uejmnwlp",
+    "TWITCH_SECRET": "leu03s2vel6usi6yhgxj6gib85x2ve"
 }
 
 module.exports = withMDX({

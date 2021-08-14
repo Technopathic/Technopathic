50c4647ab6a88b72661e068d30a16657f1fdaf9d
Ren <nsoharab@gmail.com>
2021-08-14 23:40:14 +0300

    Testing previous version

--- pages/api/generatePokemon.js
+++ pages/api/generatePokemon.js
@@ -51,14 +51,13 @@ export const getBoxes = async (req, res) => {
     })
 }
 
-const generatePokemon = (player) => {
+const generatePokemon = async (player) => {
 
     //const chain = Math.floor(Math.random() * (EVOLUTION_CHAINS) + 1)
 
     //const evolutionChain = await getEvolutionChain(chain)
     //const pokemon = await getPokemon(evolutionChain.chain.species.name)
     supabase.from('boxes').select('*').then((boxes) => {
-        console.log(boxes)
         let playerCheck = true
         if (boxes.data.length > 0) {
             boxes.data.forEach((box) => {
@@ -72,7 +71,7 @@ const generatePokemon = (player) => {
         console.log(playerCheck)
         if (playerCheck) {
             const maxPokemon = Math.floor(Math.random() * (MAX_POKEMON) + 1)
-            const pokemon = getPokemon(maxPokemon)
+            const pokemon = await getPokemon(maxPokemon)
 
             const level = Math.floor(Math.random() * (MAX_LEVEL) + 1)
 
@@ -87,43 +86,43 @@ const generatePokemon = (player) => {
             const shiny = Math.random() <= (100 / 8192)
 
             const nature = NATURES[Math.floor(Math.random() * NATURES.length)]
-            const natureMod = getNature(nature)
+            const natureMod = await getNature(nature)
 
             const stats = {
                 hp: {
                     iv: hpIV,
                     ev: 0,
-                    stat: getHealthStat(pokemon.stats[0].base_stat, hpIV, level, 0),
+                    stat: await getHealthStat(pokemon.stats[0].base_stat, hpIV, level, 0),
                     base_stat: pokemon.stats[0].base_stat
                 },
                 attack: {
                     iv: attackIV,
                     ev: 0,
-                    stat: getStat(pokemon.stats[1].base_stat, attackIV, level, natureMod.attack, 0),
+                    stat: await getStat(pokemon.stats[1].base_stat, attackIV, level, natureMod.attack, 0),
                     base_stat: pokemon.stats[1].base_stat
                 },
                 defense: {
                     iv: defenseIV,
                     ev: 0,
-                    stat: getStat(pokemon.stats[2].base_stat, defenseIV, level, natureMod.defense, 0),
+                    stat: await getStat(pokemon.stats[2].base_stat, defenseIV, level, natureMod.defense, 0),
                     base_stat: pokemon.stats[2].base_stat
                 },
                 spAttack: {
                     iv: spAttackIV,
                     ev: 0,
-                    stat: getStat(pokemon.stats[3].base_stat, spAttackIV, level, natureMod.spAttack, 0),
+                    stat: await getStat(pokemon.stats[3].base_stat, spAttackIV, level, natureMod.spAttack, 0),
                     base_stat: pokemon.stats[3].base_stat
                 },
                 spDefense: {
                     iv: spDefenseIV,
                     ev: 0,
-                    stat: getStat(pokemon.stats[4].base_stat, spDefenseIV, level, natureMod.spDefense, 0),
+                    stat: await getStat(pokemon.stats[4].base_stat, spDefenseIV, level, natureMod.spDefense, 0),
                     base_stat: pokemon.stats[4].base_stat
                 },
                 speed: {
                     iv: speedIV,
                     ev: 0,
-                    stat: getStat(pokemon.stats[5].base_stat, speedIV, level, natureMod.speed, 0),
+                    stat: await getStat(pokemon.stats[5].base_stat, speedIV, level, natureMod.speed, 0),
                     base_stat: pokemon.stats[5].base_stat
                 }
             }
@@ -139,7 +138,7 @@ const generatePokemon = (player) => {
                 }
             }
 
-            const berry = getBerry(Math.floor(Math.random() * (MAX_BERRIES) + 1))
+            const berry = await getBerry(Math.floor(Math.random() * (MAX_BERRIES) + 1))
 
             const pokemonData = {
                 egg: false,
@@ -167,12 +166,12 @@ const generatePokemon = (player) => {
                 let activeBox = boxes.data.find(box => box.pokemon.length < 30)
                 if (activeBox) {
                     activeBox.push(pokemonData)
-                    supabase.from('boxes').update(activeBox).eq('id', activeBox.id)
+                    await supabase.from('boxes').update(activeBox).eq('id', activeBox.id)
                 } else {
-                    supabase.from('boxes').insert([{ name: 'Poke Box', pokemon: [pokemonData] }])
+                    await supabase.from('boxes').insert([{ name: 'Poke Box', pokemon: [pokemonData] }])
                 }
             } else {
-                supabase.from('boxes').insert([{ name: 'Poke Box', pokemon: [pokemonData] }])
+                await supabase.from('boxes').insert([{ name: 'Poke Box', pokemon: [pokemonData] }])
             }
 
             return pokemonData
@@ -206,7 +205,7 @@ export default async (req, res) => {
         console.log("Successful Verification")
     }
 
-    generatePokemon(player)
+    await generatePokemon(player)
 
     return res.status(200).json({ success: "OK" })
 

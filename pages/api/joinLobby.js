0aaf9e701b8f8f5a727b0ecceb2fd0324b603e45
ren <ren.soharab@quicksave.fi>
2022-10-17 20:17:43 +0300

    Updated offsets

---
+++ pages/api/joinLobby.js
@@ -0,0 +1,161 @@
+import { createClient } from '@supabase/supabase-js'
+import * as BLOCK_MATRIX from '../../data/blocks.json';
+//const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+
+const MAX_GAME_LENGTH = 15
+const MAX_BLOCKS = 16
+const BLOCK_TYPES = []
+
+const getLobby = async(lobbyId) => {
+    const { data, error } = await supabase.from('lobby').select('*').eq('id', lobbyId)
+    if(error) {
+        return undefined
+    }
+
+    return data[0].lobby
+}
+
+const getGame = async(lobbyId)  => {
+    const { data, error } = await supabase.from('game').select('*').eq('lobbyId', lobbyId);
+    if(error) {
+        return undefined
+    }
+
+    return data[0].game;
+}
+
+export default async (req, res) => {
+    if (req.method !== 'POST') {
+        return res.status(401).json({ error: 'Not Allowed' })
+    }
+
+    const currentDate = new Date();
+
+    const requestData = JSON.parse(req.body)
+    const playerName = requestData.playerName
+    const lobbyId = requestData.lobbyId
+    const teamName = requestData.teamName
+
+    if(!playerName || !lobbyId || !teamName) {
+        return res.status(401).json({
+            error: req.body
+        })
+    }
+
+    const lobby = await getLobby(lobbyId);
+    if(!lobby) {
+        return res.status(404).json({
+            error: `Lobby ${lobbyId} not found.`
+        })
+    }
+    
+    const team = lobby.teams.find(item => item.TEAM_NAME === teamName)
+    if(!team) {
+        return res.status(404).json({
+            error: `Team ${teamName} not found.`
+        })
+    }
+
+    if(team.PLAYERS.length === lobby.maxPlayers / 2) {
+        return res.status(401).json({
+            error: 'Team is full.'
+        })
+    }
+
+    const game = await getGame(lobby.id);
+    if(game && game.lock === true) {
+        if(((currentDate.getTime() - game.startDate.getTime()) / 1000) / 60 < MAX_GAME_LENGTH) {
+            return res.status(401).json({
+                error: 'Game is currently in session.'
+            })
+        } else {
+            game.lock = false
+            await supabase.from('ravagers').update({ game }).match({ id: game.id })
+        }
+    }
+
+    const playerExists = lobby.teams.some(team => team.PLAYERS.some(player => player.PLAYER_NAME === playerName));
+    if(playerExists) {
+        await leaveLobby(lobby, playerExists, team);
+
+        return res.status(200).json({
+            success: true,
+            message: `${playerName} left lobby.`,
+            gameStart: false,
+            teams: [],
+            blocks: []
+        })
+    }
+    
+    team.PLAYERS.push({
+        PLAYER_NAME: playerName
+    })
+
+    await supabase.from('lobby').update({ lobby }).match({ id: lobby.id })
+
+    let gameStart = false;
+    let gameData = null
+    if(lobby.teams.every(team => team.PLAYERS.length === maxPlayers / 2)) {
+        gameData = await startGame(lobby)
+        if(gameData) {
+            gameStart = true;
+        }
+    }
+
+    return res.status(200).json({
+        success: true,
+        message: `${playerName} joined lobby.`,
+        gameStart,
+        teams: gameData ? gameData.teams : null,
+        blocks: gameData ? gameData.BLOCKS : null
+    })
+}
+
+const startGame = async (lobby) => {
+    const generateBlocks = async(count, offset) => {
+        const blocks = [];
+        for (let i = 0; i < count; i++) {
+            const block = BLOCK_TYPES[Math.floor(Math.random() * BLOCK_TYPES.length)];
+            const blockPosition = BLOCK_MATRIX[`LOBBY_${lobby.id}`][i]
+            blocks.push({
+                BLOCK_NAME: block,
+                BLOCK_POSITION: {
+                    x: blockPosition.x,
+                    y: blockPosition.y,
+                    z: blockPosition.z + offset
+                },
+                HAS_BLOCK: false,
+                PLACED_AT: "",
+                PLACED_BY: ""
+            })
+        }
+
+
+        return blocks;
+    }
+
+    
+    const game = {
+        teams: lobby.teams
+    }
+
+    game.teams[0].BLOCKS = await generateBlocks(MAX_BLOCKS, -11)
+    game.teams[1].BLOCKS = await generateBlocks(MAX_BLOCKS, 11)
+    lobby.teams[0].PLAYERS = []
+    lobby.teams[1].PLAYERS = []
+
+    const { data, error } = await supabase.from('games').insert({ game })
+    await supabase.from('lobby').update({ lobby }).eq({ id: lobby.id })
+
+    return data[0].game
+}
+
+const leaveLobby = async (lobby, player, team) => {
+    for (let i = 0; i < lobby.teams[team.TEAM_NAME].PLAYERS.length; i++) {
+        if(lobby.teams[team.TEAM_NAME].PLAYERS[i].PLAYER_NAME === player.name) {
+            lobby.teams[team.TEAM_NAME].PLAYERS.splice(i, 1);
+        }
+    }
+
+    await supabase.from('lobby').update({ lobby }).match({ id: lobby.id })
+}
--- pages/api/postBlocks.js
+++ pages/api/postBlocks.js
@@ -1,19 +1,26 @@
 import { createClient } from '@supabase/supabase-js'
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
-const CURRENT_GAME = 1;
+const getGame = async(gameId) => {
+    const { data, error } = await supabase.from('games').select('*').eq('id', gameId)
+    if(error) {
+       return undefined
+    }
+    return data[0].game
+}
 
 export default async (req, res) => {
     if (req.method !== 'POST') {
         return res.status(401).json({ error: 'Not Allowed' })
     }
 
-    const requestData = JSON.parse(req.body);
-    const playerName = requestData.playerName;
-    const blockType = requestData.blockType;
-    const blockX = Number(requestData.x);
-    const blockY = Number(requestData.y);
-    const blockZ = Number(requestData.z);
+    const requestData = JSON.parse(req.body)
+    const playerName = requestData.playerName
+    const blockType = requestData.blockType
+    const blockX = Number(requestData.x)
+    const blockY = Number(requestData.y)
+    const blockZ = Number(requestData.z)
+    const gameId = requestData.gameId
 
     if(!playerName || !blockType || !blockX || !blockY || !blockZ) {
         return res.status(401).json({
@@ -21,47 +28,46 @@ export default async (req, res) => {
         })
     }
 
-    const getGame = async() => {
-        const { data, error } = await supabase.from('ravagers').select('*').eq('id', CURRENT_GAME);
-        if(error) {
-           return { game: null, currentRound: null }
-        }
-        return { game: data[0].game, currentRound: data[0].current_round }
-    }
-
-    const { game, currentRound} = await getGame();
+    const { game } = await getGame(gameId);
     if(!game) {
         return res.status(401).json({
-            success: false
+            error: 'Game not found'
         })
     }
-    const gameRound = game[`ROUND_${currentRound}`]
 
-    for (const index in gameRound) {
-        const match = gameRound[index]
-
-        const playerMatch = match.find(m => m.PLAYERS.find(player => player.PLAYER_NAME === playerName))
+    for (const team of game.teams) {
+        const playerMatch = team.PLAYERS.find(player => player.PLAYER_NAME === playerName)
         if(playerMatch) {
-            const matchIndex = match.findIndex(m => m.BLOCKS.find(block => 
+            const blockItem = team.BLOCKS.find(block => 
                 block.BLOCK_NAME === blockType && 
                 block.BLOCK_POSITION.x === blockX &&
                 block.BLOCK_POSITION.y === blockY &&
                 block.BLOCK_POSITION.z === blockZ &&
                 block.HAS_BLOCK === false
-            ));
-           
-            if(matchIndex !== null && matchIndex !== -1) {
-                const blockIndex = gameRound[index][matchIndex].BLOCKS.findIndex(b => b.BLOCK_NAME === blockType)
-                gameRound[index][matchIndex].BLOCKS[blockIndex].HAS_BLOCK = true
-                gameRound[index][matchIndex].BLOCKS[blockIndex].PLACED_BY = playerName
-                gameRound[index][matchIndex].BLOCKS[blockIndex].PLACED_AT = new Date()
-                await supabase.from('ravagers').update({ game }).match({ id: CURRENT_GAME })
-                
+            )
+
+            if(blockItem) {
+                blockItem.HAS_BLOCK = true
+                blockItem.PLACED_BY = playerName
+                blockItem.PLACED_AT = new Date()
+
+                await supabase.from('game').update({ game }).match({ id: game.id })
+
+                const endGame = team.BLOCKS.every(block => block.HAS_BLOCK === true)
+                const winner = null
+                if(endGame) {
+                    game.lock = false
+                    winner = team
+                }
+
                 return res.status(200).json({
-                    success: true
+                    success: true,
+                    endGame,
+                    winner
                 })
             }
         }
+
     }
    
     return res.status(401).json({

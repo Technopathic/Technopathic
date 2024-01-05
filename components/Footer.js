fffe4aabd02bb78be0e1c211c356ef3ed954880c
Ren <nsoharab@gmail.com>
2024-01-05 19:29:48 +0200

    remove unused files

--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-    <footer className="h-16 w-full flex flex-col justify-center items-center">
-        <div className="flex flex-row text-center text-gray-900 flex-wrap justify-center dark:text-gray-100">Made with&nbsp;<span className="text-pink-500">❤</span>&nbsp;in Helsinki</div>
-    </footer>
-)
-
-export default Footer
